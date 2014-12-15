8c2f5847b519f580bad92ea66c11bbe047120d7e
Jonathan Fok kan <jfokkan@inocybe.ca>
2014-12-15 16:25:01 -0500

    Added Java code generation experimentation

--- build.sbt
+++ build.sbt
@@ -6,11 +6,10 @@ scalaVersion := "2.11.1"
 
 resolvers += Resolver.mavenLocal
 
-// Change this to another test framework if you prefer
 libraryDependencies += "org.scalatest" %% "scalatest" % "2.1.6" % "test"
 
 libraryDependencies += "org.opendaylight.yangtools" % "yang-parser-impl" % "0.7.0-SNAPSHOT"
 
-// Uncomment to use Akka
-//libraryDependencies += "com.typesafe.akka" % "akka-actor_2.11" % "2.3.3"
+libraryDependencies += "org.opendaylight.yangtools" % "maven-sal-api-gen-plugin" % "0.7.0-SNAPSHOT"
 
+libraryDependencies += "com.eed3si9n" %% "treehugger" % "0.3.0"
--- src/main/scala/com/inocybe/ScalaYangTools.scala
+++ src/main/scala/com/inocybe/ScalaYangTools.scala
@@ -2,15 +2,27 @@ package com.inocybe
 
 import java.io.File
 
+import scala.collection.JavaConverters._
 import scala.io.Source
 import org.opendaylight.yangtools.yang.parser.impl.YangParserImpl
+import org.opendaylight.yangtools.maven.sal.api.gen.plugin.CodeGeneratorImpl
+import org.sonatype.plexus.build.incremental.DefaultBuildContext
 
 object ScalaYangTools {
   def main(args: Array[String]): Unit = {
-    //println("yang : \n" + readFile("test.yang"))
-    val parser = YangParserImpl.getInstance()
-    val yangFile = new File("yangtest/test.yang")
-    val schema = parser.parseFile(yangFile, new File("yangtest"))
+    val parser = new YangParserImpl() //.getInstance()
+    val ietfTypeFiles = new File("yangtest/ietf").listFiles()
+    //val yangFiles = List(new File("yangtest/test.yang"), new File("yangtest/binary-type-test.yang"), new File("yangtest/binary-types@2013-06-13.yang")) ++ ietfTypeFiles.toList
+    val yangFiles = List(new File("yangtest/binary-types@2013-06-13.yang"))
+    //val schema = parser.parseFile(yangFile, new File("yangtest")
+    val schemaContext = parser.parseFiles( yangFiles.asJava )
+    println(schemaContext.toString)
+
+    val codeGen = new CodeGeneratorImpl()
+    val bctx = new DefaultBuildContext()
+    codeGen.setBuildContext(bctx)
+    val files = codeGen.generateSources(schemaContext, new File("yangtest/output"),schemaContext.getModules )
+
   }
 
   def readFile(yangfile: String): String = {
